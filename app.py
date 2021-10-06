7d0f26fbf67ac5f0b2142152bc158aec79177114
cjmilner73 <chris@blueemoon.sg>
2021-10-06 18:49:37 +0800

    commit

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ import requests
 import psycopg2
 from configparser import ConfigParser
 import os
+import boto3
+import argparse
+import base64
+import json
+import logging
+from pprint import pprint
+import time
+import boto3
+from botocore.exceptions import ClientError
 # import mysecret
 
 
 app = Chalice(app_name='helloworld')
 
 
-@app.route('/')
+@app.route('/holdings')
 def index():
     path = os.getcwd()
     result = connect()
@@ -18,7 +27,8 @@ def index():
 
 def get_secret():
 
-    secret_name = "tracker_db_secret"
+#    secret_name = "tracker_db_secret"
+    secret_name = "database"
     region_name = "ap-southeast-1"
 
     # Create a Secrets Manager client
@@ -31,54 +41,73 @@ def get_secret():
     # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
     # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     # We rethrow the exception by default.
-
+    print("SM client...")
+    print(client)
     try:
+        print("Trying client.get_secret_value")
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
+        print("Got client.get_secret_value")
     except ClientError as e:
+        print("Response e.response:")
+        print(e.response)
         if e.response['Error']['Code'] == 'DecryptionFailureException':
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
             # Deal with the exception here, and/or rethrow at your discretion.
+            print("Failed:1 ")
             raise e
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
             # An error occurred on the server side.
             # Deal with the exception here, and/or rethrow at your discretion.
+            print("Failed:2 ")
             raise e
         elif e.response['Error']['Code'] == 'InvalidParameterException':
             # You provided an invalid value for a parameter.
             # Deal with the exception here, and/or rethrow at your discretion.
+            print("Failed:3 ")
             raise e
         elif e.response['Error']['Code'] == 'InvalidRequestException':
             # You provided a parameter value that is not valid for the current state of the resource.
             # Deal with the exception here, and/or rethrow at your discretion.
+            print("Failed:4 ")
             raise e
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
             # We can't find the resource that you asked for.
             # Deal with the exception here, and/or rethrow at your discretion.
+            print("Failed:5 ")
             raise e
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        print("Should be returning Secret now")
         if 'SecretString' in get_secret_value_response:
             secret = get_secret_value_response['SecretString']
+        #    print("Secret is" + secret)
+            print(type(secret))
             return json.loads(secret)
         else:
             decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-
+    finally:
+        print("Finally")
     # Your code goes here.
 
 def connect():
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
-        myPass = mysecret.get_secret()
-        # print(myPass[passw])
+        print("Calling get_secret()")
+        mySecStr = get_secret()
+        print("mySecStr password is: " + mySecStr["password"])
+        myPass = mySecStr["password"]
 
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect("host=trackerdb.cluster-cnylyxuhkuui.ap-southeast-1.rds.amazonaws.com dbname=postgres user=app_user password=${myPass}")
-        # conn = psycopg2.connect("host=localhost dbname=tracker user=app_user password=${myPass}")
+        connStr = f"host=database-1.cluster-cnylyxuhkuui.ap-southeast-1.rds.amazonaws.com dbname=postgres user=app_user password={myPass}"
+        #connStr = f"host=trackerdb.cluster-cnylyxuhkuui.ap-southeast-1.rds.amazonaws.com dbname=postgres user=app_user password={myPass}"
+        print("Connection String: " + connStr)
+        conn = psycopg2.connect(connStr)
+        # conn = psycopg2.connect("host=localhost dbname=tracker user=app_user password={myPass}")
 		
         # create a cursor
         cur = conn.cursor()
@@ -97,7 +126,8 @@ def connect():
         cur.close()
         return(db_version)
     except (Exception, psycopg2.DatabaseError) as error:
-        #print(error)
+        print(type(error))
+        print(str(error))
         print("Return error")
         return(str(error))
     finally:
